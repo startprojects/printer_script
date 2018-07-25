8db9e84560722c6e01691fa359d30198545608c1
florian <florian.jeanmart@gmail.com>
2018-07-25 14:17:12 +0200

    - clean

--- mainScript.js
+++ mainScript.js
@@ -99,7 +99,7 @@ const print = function (fileName, callback) {
 
 // PUSHER : pong
 const sendPong = function (channelForResponse) {
-    returnToResponseChannel(channelForResponse, 'pong', 'Pong! Device ' + deviceId + ' is online !!! 4');
+    returnToResponseChannel(channelForResponse, 'pong', 'Pong! Device ' + deviceId + ' is online !!! 5');
 };
 
 // PUSHER : log
