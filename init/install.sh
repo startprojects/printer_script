ccf0e69be94b48da2ba7b9d2bf93b352ab56896f
florian <florian.jeanmart@gmail.com>
2018-08-31 15:49:37 +0200

    - install script

--- init/install.sh
+++ init/install.sh
@@ -14,32 +14,12 @@
 # install printer tool
 sudo apt-get update
 sudo apt-get  install cups-client
+# !!! wrong printer version : the task are remove when there is no paper into the printer
 # follow this tutorial  : https://www.hmazter.com/2013/05/raspberry-pi-printer-server-for-labelwriter/
 # define printer as main printer
 lpadmin -d DYMO_LabelWriter_450
 # install node
 sudo apt-get install nodejs
-
-# mysql
-sudo apt-get install mysql-server
-# change root password
-# sudo mysqladmin -u root password
-# change password
-sudo mysql -u root
-GRANT ALL PRIVILEGES on *.* to 'root'@'localhost' IDENTIFIED BY 'root';
-FLUSH PRIVILEGES;
-# create DB /  table
-create database printer;
-use printer;
-create table print_task(
-   id  bigint(20) NOT NULL AUTO_INCREMENT,
-   printTaskId  bigint(20) NOT NULL,
-   fileName VARCHAR(255) NOT NULL,
-   status VARCHAR(255) NOT NULL,
-   printerTaskId VARCHAR(255),
-   PRIMARY KEY ( id )
-);
-
 
 # confirgure the timzone
 sudo timedatectl set-timezone Europe/Brussels
