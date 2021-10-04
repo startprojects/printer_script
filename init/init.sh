1caa54009b68d4ea08ec3a89c8e5c71fc45c3aac
Florian <florian.jeanmart@gmail.com>
2021-10-04 15:55:24 +0200

    - test

--- init/init.sh
+++ init/init.sh
@@ -59,14 +59,14 @@ sudo chmod 777 "$LOG_PATH"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s
-"${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+sudo "/home/pi/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-nvm install 10
+$NVM_DIR/nvm.sh install 10
 
-nvm alias default 10
+$NVM_DIR/nvm.sh alias default 10
 
-nvm use 10
+$NVM_DIR/nvm.sh use 10
 
 node -v
 
